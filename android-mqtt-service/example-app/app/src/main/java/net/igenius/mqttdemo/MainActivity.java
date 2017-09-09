a79f2f3f03bb65dc802fef2e3a344b20cb508e3a
Rodrigo Loza <lozuwaucb@gmail.com>
2017-09-09 17:42:10 -0400

    type: feature description: mqtt service

---
+++ android-mqtt-service/example-app/app/src/main/java/net/igenius/mqttdemo/MainActivity.java
@@ -0,0 +1,100 @@
+package net.igenius.mqttdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import net.igenius.mqttservice.MQTTService;
+import net.igenius.mqttservice.MQTTServiceCommand;
+import net.igenius.mqttservice.MQTTServiceLogger;
+import net.igenius.mqttservice.MQTTServiceReceiver;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MQTTServiceReceiver receiver = new MQTTServiceReceiver() {
+
+        private static final String TAG = "Receiver";
+
+        @Override
+        public void onSubscriptionSuccessful(Context context, String requestId, String topic) {
+            Log.e(TAG, "Subscribed to " + topic);
+
+            JsonObject request = new JsonObject();
+            request.addProperty("question", "best time to post");
+            request.addProperty("lang", "en");
+            request.addProperty("request_uid", "testAndroid/" + new Date().getTime());
+
+            byte[] payload = new Gson().toJson(request).getBytes();
+
+            MQTTServiceCommand.publish(context, "/advisor/topic", payload);
+        }
+
+        @Override
+        public void onSubscriptionError(Context context, String requestId, String topic, Exception exception) {
+            Log.e(TAG, "Can't subscribe to " + topic, exception);
+        }
+
+        @Override
+        public void onPublishSuccessful(Context context, String requestId, String topic) {
+            Log.e(TAG, "Successfully published on topic: " + topic);
+        }
+
+        @Override
+        public void onMessageArrived(Context context, String topic, byte[] payload) {
+            Log.e(TAG, "New message on " + topic + ":  " + new String(payload));
+        }
+
+        @Override
+        public void onConnectionSuccessful(Context context, String requestId) {
+            Log.e(TAG, "Connected!");
+        }
+
+        @Override
+        public void onException(Context context, String requestId, Exception exception) {
+            exception.printStackTrace();
+            Log.e(TAG, requestId + " exception");
+        }
+
+        @Override
+        public void onConnectionStatus(Context context, boolean connected) {
+
+        }
+    };
+
+    String server = "ssl://yourserver.com:port";
+    String username = "username";
+    String password = "password";
+    String clientId = UUID.randomUUID().toString();
+    String publishTopic = "/some/publish/topic";
+    String subscribeTopic = "/some/subscribe/topic";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MQTTService.NAMESPACE = "net.igenius.mqttdemo";
+        MQTTServiceLogger.setLogLevel(MQTTServiceLogger.LogLevel.DEBUG);
+        MQTTServiceCommand.connectAndSubscribe(this, server, clientId, username, password, 0, true, subscribeTopic);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        receiver.unregister(this);
+    }
+}
+
